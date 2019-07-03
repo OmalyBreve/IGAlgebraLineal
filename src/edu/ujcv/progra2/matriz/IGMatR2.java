3f3dcd34d82f76e59042e15c078b8112b0f6db81
omalybreve <omalybreve@yahoo.es>
2019-07-03 08:36:08 -0600

    Agregando comentarios

--- src/edu/ujcv/progra2/matriz/IGMatR2.java
+++ src/edu/ujcv/progra2/matriz/IGMatR2.java
@@ -1,5 +1,58 @@
 package edu.ujcv.progra2.matriz;
 
+import edu.ujcv.progra2.vector.IGVecR2;
+import edu.ujcv.progra2.vector.IGVecR3;
+
 public class IGMatR2 {
     // TODO: implementar.
+
+    //double x1,y1  ,z1, w1;
+    //double x2,y2  ,z2, w2;
+
+    //double x3,y3,z3   ,w3;
+
+    //double x4,y4,z4,w4;
+
+
+    //double[][] m;
+
+    //IGVecR2 f1;
+    //IGVecR2 f2;
+    //IGVecR3 f3;
+    //IGVecR4 f4;
+
+
+    //IGVecR2 col1;
+    //IGVecR2 col2;
+    //Utilizar col
+
+
+//    public IGMatR2 mul(IGMatR2 b){
+//
+//    }
+
+
+//    public IGVecR3 colXMat(IGVecR2 col){
+//
+//    }
+
+//    public IGMatR2 suma (IGMatR2 b){
+//
+//    }
+
+//    public IGMatR2 escalarXmat(double escalar){
+//
+//    }
+
+//    public static IGVecR2 rotacion (double angulo, IGVecR2 vector ){
+//
+//    }
+
+
+
+
+
+
+
 }
+
