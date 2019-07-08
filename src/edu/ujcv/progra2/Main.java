9b2c358554bd7ab363afe22b4c15bade0ee121cc
omalybreve <omalybreve@yahoo.es>
2019-07-08 08:27:19 -0600

    Avanzando en el menu

--- src/edu/ujcv/progra2/Main.java
+++ src/edu/ujcv/progra2/Main.java
@@ -1,8 +1,25 @@
 package edu.ujcv.progra2;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Bienvenido, que operacion desea realizar?");
+        int TipoDeVector = sc.nextInt();
+
+        System.out.println("Presione 2 si desea calcular en el Vector R2");
+        System.out.println("Presione 3 si desea calcular en el Vector R3");
+        System.out.println("Presione 4 si desea calcular en el vector R4");
+
+        
+
+
+
+
+
+
     }
+
 }
